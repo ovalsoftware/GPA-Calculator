bb4c7e0f8858212531ddd43a5daf07dd9d8544fe
Arsalan Iravani <Arsalan@MacBook-Pro-Arsalan.local>
2017-05-15 19:23:09 +0400

    Initial Commit

---
+++ GPA Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GPA Calculator
+//
+//  Created by Arsalan Iravani on 15.05.17.
+//  Copyright © 2017 Arsalan Iravani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
